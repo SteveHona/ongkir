db9f0bf00fdfecf8b8f8cfba2fe5314c0cf1a4a0
Zigo Stephano <zigostephano@yahoo.com>
2021-11-04 23:33:50 +0700

    UI Input pengirim dan penerima

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,8 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+  var hiddenCitySend = true.obs;
+  var provSend = "".obs;
+  var hiddenCityRecieve = true.obs;
+  var provRecieve = "".obs;
 }
---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+void main() async {
+  Uri url = Uri.parse("https://api.rajaongkir.com/starter/province");
+  final response =
+      await http.get(url, headers: {"key": "06ce6a002911ef698fca248a38f41dc3"});
+  print(response.body);
+}
